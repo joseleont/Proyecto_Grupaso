e28f9bafac8c9baf0fcf43b5f61ea032119731c5
“José <jose.leont@pucp.pe>
2021-01-01 10:34:55 -0500

    Inicial

---
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/PantallaInicialCliente.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.proyecto_grupaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInicialCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicial_cliente);
+    }
+}
---
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/PantallaInicialUsuarioTI.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.proyecto_grupaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInicialUsuarioTI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicial_usuario_t_i);
+    }
+}
