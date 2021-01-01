d95fe6c31ddb171711d1cae53e1e6aa4e965ab28
“José <jose.leont@pucp.pe>
2021-01-01 10:25:59 -0500

    pantallaInicio

---
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/InciarSesion.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.proyecto_grupaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InciarSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inciar_sesion);
+    }
+}
