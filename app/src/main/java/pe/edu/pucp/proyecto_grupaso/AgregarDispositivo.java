77ba4b1989cb75f816e81186054ed7716d422c36
“José <jose.leont@pucp.pe>
2021-01-01 11:54:17 -0500

    Pantalla agregar Dispositivo

---
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarDispositivo.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.proyecto_grupaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarDispositivo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_dispositivo);
+    }
+}
