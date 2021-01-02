3a7e0de680fa910ba136d86d7230f3a304b156f2
Stephany <a20151164@pucp.pe>
2021-01-01 21:15:59 -0500

    tomarFoto

--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarDispositivo.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarDispositivo.java
@@ -1,26 +1,21 @@
 package pe.edu.pucp.proyecto_grupaso;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AgregarDispositivo extends AppCompatActivity {
-
-    String textSpinner;
-=======
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import pe.edu.pucp.proyecto_grupaso.models.Dispositivo;
 
 public class AgregarDispositivo extends AppCompatActivity {
 
@@ -30,47 +25,14 @@ public class AgregarDispositivo extends AppCompatActivity {
     EditText etIncluye;
     EditText etCantidad;
     Spinner spinnerTipo;
-    String tipo;
-    int cantidad;
->>>>>>> f1431e9f327e8b56655ea3fc715e9db8d668371a
+    String linkImagen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_dispositivo);
 
-<<<<<<< HEAD
-
-
-
-
-
-
-        final Spinner spinner = (Spinner) findViewById(R.id.spinnerDispositivosAgregar);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.Dispositivos, android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-
-                EditText textOtro=findViewById(R.id.editTextOtro);
-                if(adapterView.getItemAtPosition(position).toString().equals("Otro")){
-                    //poner viisble el editText para que se ingrese el nuevo dispositivo
-                    textOtro.setVisibility(View.VISIBLE);
-                }else{
-                    textOtro.setVisibility(View.GONE);
-                }
-                textSpinner=adapterView.getItemAtPosition(position).toString();
-
-            }
-        });
-
-
-=======
         etOtro = findViewById(R.id.etOtros);
         etMarca = findViewById(R.id.etMarca);
         etCaracteristicas = findViewById(R.id.etCaracteristicas);
@@ -81,35 +43,64 @@ public class AgregarDispositivo extends AppCompatActivity {
     }
 
     public void AgregarFoto (View view){
+        Intent i = new Intent (this, AgregarFotoDispositivo.class);
+        int request = 1000;
+        startActivityForResult(i,request);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            linkImagen = data.getStringExtra("imagenLink");
+        }
     }
 
     public void AgregarDispositivo (View view){
+        Dispositivo miDispositivo = new Dispositivo();
+        String marca = etMarca.getText().toString();
+        String caracteristicas = etCaracteristicas.getText().toString();
+        String incluye = etIncluye.getText().toString();
+        String tipo = spinnerTipo.getSelectedItem().toString();
+        if (tipo.equals("Otros")){
+            tipo = etOtro.getText().toString();
+        }
+        String cantidad = etCantidad.getText().toString();
 
+        if (!marca.isEmpty() &&
+                !caracteristicas.isEmpty() &&
+                !incluye.isEmpty() &&
+                !cantidad.isEmpty() &&
+                !tipo.isEmpty() &&
+                !linkImagen.isEmpty()){
+
+            miDispositivo.setFoto(linkImagen);
+            miDispositivo.setTipo(tipo);
+            miDispositivo.setMarca(marca);
+            miDispositivo.setCaracteristicas(caracteristicas);
+            miDispositivo.setStock(Integer.parseInt(cantidad));
+            miDispositivo.setIncluye(incluye);
+
+            subirDispositivo(miDispositivo);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Complete todos los campos antes de agregar", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    public void subirDispositivo(Dispositivo dispositivo){
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.child("Equipos").push().setValue(dispositivo).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "Dispositivo agregado", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
 
     public  void Cancelar (View view){
         finish();
->>>>>>> f1431e9f327e8b56655ea3fc715e9db8d668371a
     }
-
-
-
-
-    public void agregarNuevoDispositivo(View view){
-
-        Button btnAgregarNuevoDipositivo=findViewById(R.id.btnAgregarNuevoDipositivo);
-        EditText editMarca=findViewById(R.id.editTextMarca);
-        String marca=editMarca.getText().toString();
-
-    }
-
-
-
-
-
-
-
-
 }
--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarFotoDispositivo.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarFotoDispositivo.java
@@ -1,14 +1,129 @@
 package pe.edu.pucp.proyecto_grupaso;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 public class AgregarFotoDispositivo extends AppCompatActivity {
+
+    ImageView imageViewFoto;
+    Bitmap imagen;
+    Button btnSubirFoto;
+    Button btnSubirCarrete;
+    Button btnTomarFoto;
+    Button btnCarrete;
+    EditText etnombreFoto;
+    StorageReference referenciaFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_foto_dispositivo);
+
+        imageViewFoto = findViewById(R.id.imageViewFoto);
+        btnSubirFoto = findViewById(R.id.btnSubirFoto);
+        btnSubirCarrete = findViewById(R.id.btnSubirCarrete);
+        btnTomarFoto = findViewById(R.id.btnTomarFoto);
+        btnCarrete = findViewById(R.id.btnCarrete);
+        etnombreFoto = findViewById(R.id.etNombreFoto);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void permisoFoto(View view) {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1000);
+        }else{
+            tomarFoto();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                tomarFoto();
+            } else {
+                Toast.makeText(this, "No hay permisos de cámara", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void tomarFoto(){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            imagen = (Bitmap) data.getExtras().get("data");
+            imageViewFoto.setImageBitmap(imagen);
+            btnSubirFoto.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void subirFotoFirebase(View view){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
+        ByteArrayOutputStream fotoJPG = new ByteArrayOutputStream();
+        imagen.compress(Bitmap.CompressFormat.JPEG, 100, fotoJPG);
+        byte cadenabytes[] = fotoJPG.toByteArray();
+
+        String nombreArchivo = etnombreFoto.getText().toString();
+        referenciaFinal = storageRef.child("FotosEquipos/" + nombreArchivo);
+
+        referenciaFinal.putBytes(cadenabytes)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getApplicationContext(), "Foto subida exitosamente", Toast.LENGTH_SHORT).show();
+                        referenciaFinal.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                String miURL = task.getResult().toString();
+                                Intent i = new Intent ();
+                                i.putExtra("imagenLink", miURL);
+                                setResult(RESULT_OK, i);
+                                finish();
+                            }
+                        });
+
+                    }
+                });
+    }
+
+    public void elegirCarrete(View view){
+        //Seleccionar de carrete
+    }
+
+
 }
