7f9cf7ae522d8f2a218e89df98cb7360b7b039a2
“José <jose.leont@pucp.pe>
2021-01-01 20:32:34 -0500

    registro

--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarDispositivo.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/AgregarDispositivo.java
@@ -2,13 +2,72 @@ package pe.edu.pucp.proyecto_grupaso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AgregarDispositivo extends AppCompatActivity {
+
+    String textSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_dispositivo);
+
+
+
+
+
+
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinnerDispositivosAgregar);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Dispositivos, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                EditText textOtro=findViewById(R.id.editTextOtro);
+                if(adapterView.getItemAtPosition(position).toString().equals("Otro")){
+                    //poner viisble el editText para que se ingrese el nuevo dispositivo
+                    textOtro.setVisibility(View.VISIBLE);
+                }else{
+                    textOtro.setVisibility(View.GONE);
+                }
+                textSpinner=adapterView.getItemAtPosition(position).toString();
+
+            }
+        });
+
+
     }
+
+
+
+
+    public void agregarNuevoDispositivo(View view){
+
+        Button btnAgregarNuevoDipositivo=findViewById(R.id.btnAgregarNuevoDipositivo);
+        EditText editMarca=findViewById(R.id.editTextMarca);
+        String marca=editMarca.getText().toString();
+
+    }
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/MainActivity.java
@@ -1,7 +1,10 @@
 package pe.edu.pucp.proyecto_grupaso;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void registrar(View view){
+
+        startActivity(new Intent(MainActivity.this,RegistroNuevoCliente.class));
+
+    }
+
+
 }
