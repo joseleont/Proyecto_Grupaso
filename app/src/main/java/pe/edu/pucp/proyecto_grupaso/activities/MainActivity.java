93f5539005cafe18167ec587002808946eba630a
“José <jose.leont@pucp.pe>
2021-01-02 21:43:50 -0500

    menu

--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/activities/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/activities/MainActivity.java
@@ -1,10 +1,18 @@
 package pe.edu.pucp.proyecto_grupaso.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import pe.edu.pucp.proyecto_grupaso.IniciarSesion;
 import pe.edu.pucp.proyecto_grupaso.R;
@@ -16,6 +24,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+
+               irActivityUsuarioClientre(currentUser);
+
+        }
+
+
+    }
+
+    public void irActivityUsuarioClientre(FirebaseUser currentUser){
+        if((currentUser.getEmail().equals("jose.leont@pucp.pe"))||(currentUser.getEmail().equals("a20151164@pucp.pe"))){
+            startActivity(new Intent(MainActivity.this, ClienteActivity.class));
+
+        }else{
+            startActivity(new Intent(MainActivity.this, UsuarioTIActivity.class));
+
+        }
+        finish();
     }
 
     public void registrar(View view){
