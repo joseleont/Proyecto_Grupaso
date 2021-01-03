e50fb694e41c04149bc15df5f21b012501afb736
“José <jose.leont@pucp.pe>
2021-01-02 23:06:57 -0500

    login

--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/activities/UsuarioTIActivity.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/activities/UsuarioTIActivity.java
@@ -1,8 +1,15 @@
 package pe.edu.pucp.proyecto_grupaso.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import pe.edu.pucp.proyecto_grupaso.R;
 
@@ -13,4 +20,37 @@ public class UsuarioTIActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicial_usuario_t_i);
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.menuSalir:
+                AuthUI instance = AuthUI.getInstance();
+                instance.signOut(this).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        startActivity(new Intent(UsuarioTIActivity.this,MainActivity.class));
+                        finish();
+                    }
+                });
+
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
