54dfacedabeda0a5f27da5d227e78f3d08afa906
“José <jose.leont@pucp.pe>
2021-01-06 16:33:11 -0500

    final

--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/Reservar.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/Reservar.java
@@ -1,6 +1,9 @@
 package pe.edu.pucp.proyecto_grupaso;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,8 +13,19 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +35,7 @@ import pe.edu.pucp.proyecto_grupaso.models.Solicitud;
 
 public class Reservar extends AppCompatActivity {
 
+    private GoogleMap mMap;
 
     EditText etDireccion;
     EditText etMotivo;
@@ -42,6 +57,7 @@ public class Reservar extends AppCompatActivity {
 
         Log.d("myapp", "onCreate: " + devuid + " " + usrmail);
 
+        obtenerGPS();
 
         etDireccion = findViewById(R.id.etDireccionR);
         etMotivo = findViewById(R.id.etMotivoR);
@@ -49,6 +65,7 @@ public class Reservar extends AppCompatActivity {
         gps = findViewById(R.id.etGPS);
 
         gps.setText("aqui debe ir la ubicacion gps");
+
 
 
         reservar = findViewById(R.id.btnReserva);
@@ -84,4 +101,43 @@ public class Reservar extends AppCompatActivity {
             }
         });
     }
+
+
+    public void obtenerGPS(){
+
+
+        FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        locationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+
+                //Se obtiene la ubicacion
+                // LatLng ubicacion2 = new LatLng(location.getLatitude(), location.getLongitude());
+                //  Log.d("InfoApp",location.getLatitude()+"/"+location.getLongitude());
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+
+    }
+
+
 }
