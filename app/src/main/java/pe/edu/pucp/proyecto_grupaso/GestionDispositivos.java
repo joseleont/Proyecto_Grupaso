fc6dd3962ccf6db59cabc7067bf94deda2159b5c
“José <jose.leont@pucp.pe>
2021-01-05 23:49:58 -0500

    activity"

--- app/src/main/java/pe/edu/pucp/proyecto_grupaso/GestionDispositivos.java
+++ app/src/main/java/pe/edu/pucp/proyecto_grupaso/GestionDispositivos.java
@@ -2,7 +2,11 @@ package pe.edu.pucp.proyecto_grupaso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import pe.edu.pucp.proyecto_grupaso.activities.MainActivity;
 
 public class GestionDispositivos extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class GestionDispositivos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion_dispositivos);
+    }
+
+
+    public void agregarDispositivo(View view){
+        startActivity(new Intent(GestionDispositivos.this, AgregarDispositivo.class));
+
     }
 }
